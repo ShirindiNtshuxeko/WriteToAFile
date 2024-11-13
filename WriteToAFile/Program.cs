35f1f6a25ebcbd174fda7b4452189ed4de1a8f04
User <User@LDIL-17>
2024-11-13 22:56:27 +0200

    Add project files.

---
+++ WriteToAFile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WriteToAFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string DocumentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filepath = Path.Combine(DocumentPath, "New.txt");
+
+            Console.WriteLine("Please enter your name: ");
+            string TextToWrite = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filepath, true)) 
+            {
+                writer.WriteLine(TextToWrite);
+            }
+
+            Console.WriteLine("Successfully");
+        }
+    }
+}
